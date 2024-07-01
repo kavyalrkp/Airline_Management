4e87680af598786596753a578848e834361da970
Kavya Lekha Bathula <168459830+kavyalrkp@users.noreply.github.com>
2024-07-01 17:56:02 +0530

    Add files via upload

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,39 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import User from '../models/User.js'; // Import the User model
 
+const router = express.Router();
+
+// Register user
+router.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword,
+           
+        });
+
+        // Save the user to the database
+        await newUser.save();
+
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+export default router;
