2357b4a311696e9b04d55d75b8b5c7e3e3b05df2
Kavya Lekha Bathula <168459830+kavyalrkp@users.noreply.github.com>
2024-07-01 18:04:06 +0530

    Add files via upload

--- frontend/src/router/Routers.jsx
+++ frontend/src/router/Routers.jsx
@@ -1,1 +1,24 @@
+import React from "react";
 
+import { Routes, Route, Navigate} from 'react-router-dom';
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import SearchResultList from "../pages/SearchResultList";
+import ScrollToTop from "../shared/ScrollToTop";
+
+const Routers = () => {
+  return( 
+    <>
+   <ScrollToTop/>
+    <Routes>
+        <Route path="/" element={<Navigate to="/"/>}/>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/SearchResultList" element={<SearchResultList/>} />
+    
+    </Routes>
+    </>
+  );
+
+};
+export default Routers;
